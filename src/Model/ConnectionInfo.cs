97747db220155a87819e9e91b6f9b22122740c4a
Timor Gruber <timor.gruber@gmail.com>
2023-11-18 15:07:29 +0200

    doc: `ConnectionInfo` struct

--- src/Model/ConnectionInfo.cs
+++ src/Model/ConnectionInfo.cs
@@ -2,8 +2,15 @@
 
 namespace NSerial.Model
 {
+    /// <summary>
+    /// Represents the information needed to create a serial connection.
+    /// </summary>
     public struct ConnectionInfo
     {
+        /// <summary>
+        /// Creates a new connection info by copying the values from the given connection info object. 
+        /// </summary>
+        /// <param name="other">The connection info to copy from.</param>
         public ConnectionInfo(ConnectionInfo other)
         {
             PortName = other.PortName;
@@ -14,17 +21,24 @@ namespace NSerial.Model
             Handshake = other.Handshake;
         }
 
+        /// <summary>
+        /// Checks if this connection info is equal to another connection info.
+        /// </summary>
+        /// <param name="other">The other connection info to check.</param>
+        /// <returns><c>true</c> if the connection infos are equal, <c>false</c> otherwise.</returns>
         public bool Equals(ConnectionInfo other)
         {
             return PortName == other.PortName && BaudRate == other.BaudRate && DataBits == other.DataBits &&
                    Parity == other.Parity && StopBits == other.StopBits && Handshake == other.Handshake;
         }
 
-        public override bool Equals(object obj)
+        /// <inheritdoc />
+        public override bool Equals(object? obj)
         {
             return obj is ConnectionInfo other && Equals(other);
         }
 
+        /// <inheritdoc />
         public override int GetHashCode()
         {
             unchecked
@@ -39,21 +53,56 @@ namespace NSerial.Model
             }
         }
 
+        /// <summary>
+        /// Checks if two connection infos are equal.
+        /// </summary>
+        /// <param name="left">The left connection info to check.</param>
+        /// <param name="right">The right connection info to check.</param>
+        /// <returns><c>true</c> if the connection infos are equal, <c>false</c> otherwise.</returns>
         public static bool operator ==(ConnectionInfo left, ConnectionInfo right)
         {
             return left.Equals(right);
         }
 
+        /// <summary>
+        /// Checks if two connection infos are not equal.
+        /// </summary>
+        /// <param name="left">The left connection info to check.</param>
+        /// <param name="right">The right connection info to check.</param>
+        /// <returns><c>true</c> if the connection infos are not equal, <c>false</c> otherwise.</returns>
         public static bool operator !=(ConnectionInfo left, ConnectionInfo right)
         {
             return !(left == right);
         }
 
+        /// <summary>
+        /// The name of the port to connect to.
+        /// </summary>
         public string PortName { get; set; }
+
+        /// <summary>
+        /// The baud rate (speed) of the connection.
+        /// </summary>
         public int BaudRate { get; set; }
+
+        /// <summary>
+        /// The number of data bits in each byte, if any.
+        /// </summary>
         public int? DataBits { get; set; }
-        public Parity? Parity { get; set; }
+
+        /// <summary>
+        /// The number of stop bits in each byte, if any.
+        /// </summary>
         public StopBits? StopBits { get; set; }
+
+        /// <summary>
+        /// The parity configuration of the connection, if any.
+        /// </summary>
+        public Parity? Parity { get; set; }
+
+        /// <summary>
+        /// The handshake configuration of the connection, if any.
+        /// </summary>
         public Handshake? Handshake { get; set; }
     }
 }
