272deb0c74fc079634b61c92d0f0023123cf2dd7
Timor Gruber <timor.gruber@gmail.com>
2023-11-18 15:10:42 +0200

    add factory to create serial connections
    Factories allow to abstract the real type used under the hood,
    allowing better testability.

---
+++ src/Connection/Port/ISerialConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Ports;
+using NSerial.Model;
+using Optional;
+
+namespace NSerial.Connection.Port
+{
+    /// <summary>
+    /// Creates a <see cref="ISerialConnection"/> from a <see cref="ConnectionInfo"/>.
+    /// </summary>
+    public interface ISerialConnectionFactory
+    {
+        /// <summary>
+        /// Creates a <see cref="SerialPort" /> from a <see cref="ConnectionInfo" />.
+        /// </summary>
+        /// <param name="connectionInfo">The <see cref="ConnectionInfo" /> to create connection from.</param>
+        /// <returns>A <see cref="ISerialConnection" /> if created successfully,
+        /// <see cref="Option.None{T}" /> otherwise.</returns>
+        Option<ISerialConnection> CreateSerialConnection(ConnectionInfo connectionInfo);
+    }
+}
---
+++ src/Connection/Port/SystemSerialConnectionFactory.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.IO.Ports;
+using NSerial.Model;
+using Optional;
+
+namespace NSerial.Connection.Port
+{
+    /// <summary>
+    /// Creates a <see cref="ISerialConnection" /> from a <see cref="ConnectionInfo" />.
+    /// </summary>
+    public class SystemSerialConnectionFactory : ISerialConnectionFactory
+    {
+        /// <inheritdoc />
+        public Option<ISerialConnection> CreateSerialConnection(ConnectionInfo connectionInfo)
+        {
+            try
+            {
+                var systemPort = new SerialPort(connectionInfo.PortName, connectionInfo.BaudRate);
+
+                if (connectionInfo.Parity.HasValue)
+                {
+                    systemPort.Parity = connectionInfo.Parity.Value;
+                }
+
+                if (connectionInfo.DataBits.HasValue)
+                {
+                    systemPort.DataBits = connectionInfo.DataBits.Value;
+                }
+
+                if (connectionInfo.StopBits.HasValue)
+                {
+                    systemPort.StopBits = connectionInfo.StopBits.Value;
+                }
+
+                if (connectionInfo.Handshake.HasValue)
+                {
+                    systemPort.Handshake = connectionInfo.Handshake.Value;
+                }
+
+                return Option.Some<ISerialConnection>(new SerialConnection(systemPort, connectionInfo));
+            }
+            catch (IOException)
+            {
+                return Option.None<ISerialConnection>();
+            }
+        }
+    }
+}
